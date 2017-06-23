c1195817facd5a14802e12be8b1e16a288770612
pablogmendez <pablo.guillermo.mendez@gmail.com>
2017-06-23 09:05:44 -0300

    add more features to frontend

--- frontend/js/CreadorObjetosControl.js
+++ frontend/js/CreadorObjetosControl.js
@@ -1,9 +1,33 @@
 
 routerApp.controller('CreadorObjetosControl', function($scope, $location){
-  $scope.onChange = function()
-{
-   alert("HOLA");
+	$scope.paso=1;
+	$scope.totalPasos=3;
+	$scope.mostrarFormulario=true;
+	$scope.mostrarResumen=false;
+	$scope.mostrarCreaObjetos=false;
 
-}
-	
+	$scope.resumenOperacion = function() {
+		$scope.mostrarFormulario=false;
+		$scope.mostrarResumen=true;
+		$scope.paso=2;
+	}
+
+	$scope.formularioAlta = function() {
+		$scope.mostrarFormulario=true;
+		$scope.mostrarResumen=false;
+		$scope.paso=1;
+	}
+
+	$scope.crearObjetos = function() {
+		$scope.mostrarCreaObjetos=true;
+		$scope.mostrarResumen=false;
+		$scope.paso=3;
+	}
+
+	$scope.volverAlInicio = function() {
+		$scope.mostrarFormulario=true;
+		$scope.mostrarCreaObjetos=false;
+		$scope.paso=1;
+	}
+
 });
